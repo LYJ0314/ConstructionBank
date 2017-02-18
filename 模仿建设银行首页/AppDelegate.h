f6bb734ee54c5057afbafab36a283eace377edb5
liyajun <liyajun@haodf.com>
2017-02-18 10:59:31 +0800

    模仿建设银行首页动画

---
+++ 模仿建设银行首页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  模仿建设银行首页
+//
+//  Created by 李亚军 on 17/2/18.
+//  Copyright © 2017年 李亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 模仿建设银行首页/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  模仿建设银行首页
+//
+//  Created by 李亚军 on 17/2/18.
+//  Copyright © 2017年 李亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CLOneViewController.h"
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
